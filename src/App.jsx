e501e9a67bb5a9b0d8ffeda61633b37eb56d7166
kevin <kbruno@alumnos.exa.unicen.edu.ad>
2025-12-17 12:26:01 -0300

    feat: estructurando aplicacion con screaming (estructura) para mayor organizacion y que sea mas escalable

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline text-red-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
